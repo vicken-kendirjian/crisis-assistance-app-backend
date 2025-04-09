1bcf0b00fac1fc7e7972312802bd6ac8a695c4a2
vicken-kendirjian <vicken.kendirjian@lau.edu>
2025-04-09 23:47:26 +0300

    added route

--- routes/VolunteerRoute.ts
+++ routes/VolunteerRoute.ts
@@ -1,6 +1,6 @@
 import express, {Request,Response,NextFunction} from 'express';
 import { CreateUserValidation, OTPValidation, LoginValidation, VolunteerValidation } from '../middlewares/ValidationMiddleware';
-import { ApplyAsVolunteer } from '../controllers/VolunteerController';
+import { ApplyAsVolunteer, getAcceptedApplicants } from '../controllers/VolunteerController';
 import { allowUser, AuthorizeUser } from '../middlewares/AuthMiddleware';
 const router = express.Router();
 
@@ -8,6 +8,7 @@ router.use(AuthorizeUser);
 router.use(allowUser);
 
 router.post('/apply', VolunteerValidation, ApplyAsVolunteer)
+router.get('/volunteers-accepted', getAcceptedApplicants)
 
 
 
