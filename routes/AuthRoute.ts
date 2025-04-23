4e868f4fd2037da39d4781d3e919e089e210b7d6
vicken-kendirjian <vicken.kendirjian@lau.edu>
2025-04-23 22:19:34 +0300

    my bad

--- routes/AuthRoute.ts
+++ routes/AuthRoute.ts
@@ -1,7 +1,7 @@
 import express, {Request,Response,NextFunction} from 'express';
 import { CreateUserValidation, OTPValidation, LoginValidation } from '../middlewares/ValidationMiddleware';
 import { AuthorizeUser } from '../middlewares/AuthMiddleware';
-import { userLogin, userSignup, verifyOtp, testCall, authTestCall, checkStartup } from '../controllers';
+import { userLogin, userSignup, verifyOtp, testCall, authTestCall, checkStartup, logout } from '../controllers';
 
 const router = express.Router();
 
@@ -12,7 +12,7 @@ router.post('/login', LoginValidation, userLogin)
 router.post('/check-token', AuthorizeUser, checkStartup)
 router.get('/test', testCall)
 router.get('/authotest', AuthorizeUser, authTestCall)
-
+router.post('/logout', logout)
 
 
 export {router as AuthRoute};
